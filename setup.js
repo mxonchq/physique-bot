1e5ba51fdafcda7b0c6ad1c3bedccf98ee45b8e6
mxonchq <mxonchq@gmail.com>
2026-03-16 19:05:18 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,8 +1,8 @@
 // Запусти один раз: node setup.js
 // Устанавливает webhook на Vercel
 
-const BOT_TOKEN   = "ТВОЙ_ТОКЕН";       // ← вставь токен
-const VERCEL_URL  = "ТВОЙ_ДОМЕН";       // ← вставь домен с Vercel (например: physique-bot.vercel.app)
+const BOT_TOKEN   = "8711929320:AAGkqeMqll3t9mDDTEtmJc_5Ktd2W57Azk4";       // ← вставь токен
+const VERCEL_URL  = "physique-bot.vercel.app";       // ← вставь домен с Vercel (например: physique-bot.vercel.app)
 
 fetch(`https://api.telegram.org/bot${BOT_TOKEN}/setWebhook`, {
   method: "POST",
